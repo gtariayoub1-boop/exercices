008fa0cd2f345d801a0bc261b3d2303fa227f02d
Your Name <you@example.com>
2025-10-03 17:55:34 +0100

    Exercice 1 - Instruction 3

--- src/ZooManagement2.java
+++ src/ZooManagement2.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
-public class ZooManagement1 {
+public class ZooManagement2 {
     int nbrCages;
     String zooName;
 
     public static void main(String[] args) {
 
-        ZooManagement1 zoo = new ZooManagement1();
+        ZooManagement2 zoo = new ZooManagement2();
+
+        Scanner s = new Scanner(System.in);
 
         System.out.print("Entrez le nom du zoo : ");
-        Scanner s = new Scanner(System.in);
         zoo.zooName = s.nextLine();
 
         System.out.print("Entrez le nombre de cages : ");
         zoo.nbrCages = s.nextInt();
 
+        // Premier affichage
         System.out.println("Le zoo " + zoo.zooName + " contient " + zoo.nbrCages + " cages.");
+
+        // Deuxième affichage (message demandé)
+        System.out.println(zoo.zooName + " comporte " + zoo.nbrCages + " cages.");
 
         s.close();
     }
