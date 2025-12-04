6fbc30f10d8bc55ad23335b994acb03b0943a3dc
Your Name <you@example.com>
2025-12-04 17:13:09 +0100

    Ajout du projet GestionEm sur la branche Prosit12

---
+++ src/StudentManagement.java
@@ -0,0 +1,46 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StudentManagement implements Management {
+
+    @Override
+    public void displayStudents(List<Student> students, Consumer<Student> con) {
+        students.forEach(con);
+    }
+
+    @Override
+    public void displayStudentsByFilter(List<Student> students, Predicate<Student> pre,
+                                        Consumer<Student> con) {
+        students.stream().filter(pre).forEach(con);
+    }
+
+    @Override
+    public String returnStudentsNames(List<Student> students,
+                                      Function<Student, String> fun) {
+        return students.stream()
+                .map(fun)
+                .reduce("", (a, b) -> a + b + " ");
+    }
+
+    @Override
+    public Student createStudent(Supplier<Student> sup) {
+        return sup.get();
+    }
+
+    @Override
+    public List<Student> sortStudentsById(List<Student> students,
+                                          Comparator<Student> com) {
+        students.sort(com);
+        return students;
+    }
+
+    @Override
+    public Stream<Student> convertToStream(List<Student> students) {
+        return students.stream();
+    }
+}
